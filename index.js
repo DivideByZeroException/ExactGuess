c7a1b7bc240ab807b43ba7793c7caea83094128e
Африка <106483551+DivideByZeroException@users.noreply.github.com>
2024-01-21 18:56:38 +0300

    intall discord and env

--- index.js
+++ index.js
@@ -3,9 +3,22 @@
 
 const express = require("express");
 const axios = require("axios");
+const dotenv = require("dotenv");
 
 const app = express();
 app.use(express.json());
 
 const port = 5000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
+import { Client, GatewayIntentBits } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
+
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => GatewayIntentBits[a]),
+});
+client.on('ready', () => {
+    console.log(`Бот запущен как ${client.user.tag}`);
+    //setImg();
+});
+
+client.login(process.env.DISCORD_TOKEN);
