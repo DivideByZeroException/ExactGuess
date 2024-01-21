4c39e244ece277eb5d38702c99bf1426ff1494ec
Африка <106483551+DivideByZeroException@users.noreply.github.com>
2024-01-21 19:08:36 +0300

    add functions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 import axios from 'axios';
 import express from "express"
 import dotenv from 'dotenv';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
 
 
 
@@ -13,8 +15,21 @@ app.use(express.json());
 
 const port = 5000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
-import { Client, GatewayIntentBits } from 'discord.js';
-import { EmbedBuilder } from 'discord.js';
+
+
+let iso = "";
+
+const apiKey = process.env.API_KEY_MAPILLARY;
+
+var imgURL = "";
+
+const channelId = '1198273314712473620';
+
+const secret_channel_id = '1198277832133070931';
+
+
+
+
 
 const client = new Client({
   intents: Object.keys(GatewayIntentBits).map((a) => GatewayIntentBits[a]),
@@ -23,5 +38,124 @@ client.on('ready', () => {
     console.log(`Бот запущен как ${client.user.tag}`);
     //setImg();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function sendImage() {
+    const channel = client.channels.cache.get(channelId);
+    const channel_secret = client.channels.cache.get(secret_channel_id);
+    if (channel && channel_secret) {
+        let exampleEmbed = new EmbedBuilder()
+            .setColor(generateRandomHexColor())
+            .setTitle("Угадай страну")
+            .setImage(imgURL)
+        channel.send({
+            embeds: [exampleEmbed]
+        });
+        channel_secret.send(iso);
+    } else {
+        console.error(`Канал с ID ${channelId} не найден.`);
+    }
+
+}
+
+function setImg() {
+   
+    var randomCoordinates = generateRandomCoordinates();
+    var url = `https://graph.mapillary.com/images?access_token=${apiKey}&fields=id,computed_geometry,thumb_1024_url&bbox=` + randomCoordinates.minLongitude + "," + randomCoordinates.minLatitude + "," + randomCoordinates.maxLongitude + "," + randomCoordinates.maxLatitude + "," + "&limit=1";
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            imgURL = data.data[0].thumb_1024_url;
+            var coords = data.data[0].computed_geometry.coordinates;
+            var x = coords[0];
+            var y = coords[1];
+            console.log(imgURL);
+            getCountryCodeByCoordinates(y, x, process.env.GEO_NAME)
+                .then((countryCode) => {
+                    console.log(countryCode);
+                    if (countryCode == undefined) {
+                        setImg();
+                    } 
+                    else {
+                        iso = countryCode;
+                        console.log(iso);
+                        sendImage();
+                    }
+                });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            setImg();
+        });
+};
+
+
+
+
+function getRandomCoordinate(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function generateRandomCoordinates() {
+    const centerLatitude = getRandomCoordinate(-90, 90);
+    const centerLongitude = getRandomCoordinate(-180, 180);
+    const latitudeRange = 0.25;
+    const minLatitude = Math.max(centerLatitude - latitudeRange / 2, -90);
+    const maxLatitude = Math.min(centerLatitude + latitudeRange / 2, 90);
+    const longitudeRange = 0.25;
+    const minLongitude = (centerLongitude - longitudeRange / 2 + 180) % 360 - 180;
+    const maxLongitude = (centerLongitude + longitudeRange / 2 + 180) % 360 - 180;
+
+
+    return {
+        centerLatitude,
+        centerLongitude,
+        minLatitude,
+        maxLatitude,
+        minLongitude,
+        maxLongitude,
+    };
+}
+
+
+
+async function getCountryCodeByCoordinates(lat, lng, username) {
+    const apiUrl = `http://api.geonames.org/countryCodeJSON?lat=${lat}&lng=${lng}&username=${username}`;
+
+    try {
+        const response = await axios.get(apiUrl);
+
+        if (response.data.countryCode) {
+            const countryCode = response.data.countryCode.toUpperCase();
+            return countryCode;
+        } else {
+            throw new Error('Unable to retrieve country code.');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+function generateRandomHexColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const hexColor = "#" + "0".repeat(6 - randomColor.length) + randomColor;
+    return hexColor;
+}
 
 client.login(process.env.DISCORD_TOKEN);
