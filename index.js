f9cdb80761200393fa8a254b9b23dac8f4a696c1
Африка <106483551+DivideByZeroException@users.noreply.github.com>
2024-01-21 19:17:09 +0300

    fetch comment

--- index.js
+++ index.js
@@ -61,9 +61,9 @@ client.on('messageCreate', (message) => {
 
         if (message.content.toUpperCase() == iso) {
             message.react('✅');
-           
+
             const channel = client.channels.cache.get(channelId);
- 
+
             if (channel) {
                 channel.send("<@"+message.author.id+"> Правильно угадал. Страна была "+iso + " " + String.fromCodePoint(...iso.split('').map(c => c.charCodeAt(0) + 127397))+ '. Следующая картинка на подходе!');
             }
@@ -109,35 +109,35 @@ function sendImage() {
 }
 
 function setImg() {
-   
+
     var randomCoordinates = generateRandomCoordinates();
     var url = `https://graph.mapillary.com/images?access_token=${apiKey}&fields=id,computed_geometry,thumb_1024_url&bbox=` + randomCoordinates.minLongitude + "," + randomCoordinates.minLatitude + "," + randomCoordinates.maxLongitude + "," + randomCoordinates.maxLatitude + "," + "&limit=1";
     console.log(url);
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            imgURL = data.data[0].thumb_1024_url;
-            var coords = data.data[0].computed_geometry.coordinates;
-            var x = coords[0];
-            var y = coords[1];
-            console.log(imgURL);
-            getCountryCodeByCoordinates(y, x, process.env.GEO_NAME)
-                .then((countryCode) => {
-                    console.log(countryCode);
-                    if (countryCode == undefined) {
-                        setImg();
-                    } 
-                    else {
-                        iso = countryCode;
-                        console.log(iso);
-                        sendImage();
-                    }
-                });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            setImg();
-        });
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         imgURL = data.data[0].thumb_1024_url;
+    //         var coords = data.data[0].computed_geometry.coordinates;
+    //         var x = coords[0];
+    //         var y = coords[1];
+    //         console.log(imgURL);
+    //         getCountryCodeByCoordinates(y, x, process.env.GEO_NAME)
+    //             .then((countryCode) => {
+    //                 console.log(countryCode);
+    //                 if (countryCode == undefined) {
+    //                     setImg();
+    //                 }
+    //                 else {
+    //                     iso = countryCode;
+    //                     console.log(iso);
+    //                     sendImage();
+    //                 }
+    //             });
+    //     })
+    //     .catch(error => {
+    //         console.error('Error fetching data:', error);
+    //         setImg();
+    //     });
 };
 
 
