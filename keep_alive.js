278d959ed18ee904aeaba3d8c61210d8ef276ba7
Африка <106483551+DivideByZeroException@users.noreply.github.com>
2024-01-21 21:08:09 +0300

    update keep alive

--- keep_alive.js
+++ keep_alive.js
@@ -1,5 +1,9 @@
+const keep_alive = () => {
 var http = require('http');
 http.createServer(function(req,res){
     res.write("alive");
     res.end();
 }).listen(8080);
+};
+
+export default keep_alive
