1137f013b68478d652d6a2b3141c08d04df4b52a
Африка <106483551+DivideByZeroException@users.noreply.github.com>
2024-01-21 18:40:37 +0300

    start

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-// Routes
-app.use("/home", home);
-
-// connection
 const port = 5000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
